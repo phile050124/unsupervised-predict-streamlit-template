2451e63b3aad0953d60212e57c799f24febd0b1b
phile050124 <148635989+phile050124@users.noreply.github.com>
2024-04-09 10:53:33 +0200

    Add files via upload

--- edsa_recommender4.py
+++ edsa_recommender4.py
@@ -141,16 +141,50 @@ def make_histogram(dataset, attribute, bins=25, bar_color='#3498db', edge_color=
 
     plt.xticks(rotation=45)
 
+def view_profile(name, email, age, gender):
+    st.write("View Profile:")
+    st.write(f"Name: {name}")
+    st.write(f"Email: {email}")
+    st.write(f"Age: {age}")
+    st.write(f"Gender: {gender}")
+
+def edit_profile():
+    st.write("Edit Profile:")
+    name = st.text_input("Name", "John Doe")
+    email = st.text_input("Email", "john@example.com")
+    age = st.number_input("Age", min_value=18, max_value=100, value=30)
+    gender = st.radio("Gender", ["Male", "Female"])
+    return name, email, age, gender
+
 
 
 # ------------------------------ CODE FOR THE FIGURES ENDS HERE ------------------------------------#
 
 # App declaration
 def main():
+    html_template = """
+        <div style="background-color:black;padding:10px;border-radius:10px;margin:10px;">
+        <h1 style="color:green;text-align:center;">EDSA Movie Recommendation Challenge</h1>
+        <h2 style="color:white;text-align:center;">UNSUPERVISED LEARNING PREDICT - TEAM FM3</h2>
+        </div>
+        """
+
+    title_template = """
+        <div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:20px;">
+        <h1 style="color:white;text-align:center;">UNSUPERVISED LEARNING PREDICT</h1>
+        <h2 style="color:white;text-align:center;">TEAM FM3</h2>
+        <h3 style="color:white;text-align:center;">Nkosingiphile Sefodi</h3>
+        <h3 style="color:white;text-align:center;">Zinhle Mjwara</h3>
+        <h3 style="color:white;text-align:center;">Jean Rabothata</h3>
+        <h3 style="color:white;text-align:center;">Thabang Ntuli </h3>
+        <h3 style="color:white;text-align:center;">Anele</h3>
+        <h3 style="color:white;text-align:center;">Nompumeza</h3>
+        </div>
+        """
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","EDA", "Solution Overview", "About Us"]
+    page_options = ["Home","Recommender System","About","EDA", "Solution Overview", "About Us"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -205,6 +239,61 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    if page_selection == "Home":
+        st.markdown(html_template.format('royalblue', 'white'), unsafe_allow_html=True)
+        st.image('resources/imgs/Home.PNG', use_column_width=True)
+        # st.markdown(title_template, unsafe_allow_html=True)
+
+    if page_selection == "About":
+        # markup(page_selection)
+        st.write("### Oveview: Flex your Unsupervised Learning skills to generate movie recommendations")
+
+        # You can read a markdown file from supporting resources folder
+        # if st.checkbox("Introduction"):
+        st.subheader("Introduction to Unsupervised Learning Predict")
+        st.write(
+            """In today’s technology driven world, recommender systems are socially and economically critical for ensuring that individuals can make appropriate choices surrounding the content they engage with on a daily basis. One application where this is especially true surrounds movie content recommendations; where intelligent algorithms can help viewers find great titles from tens of thousands of options.""")
+        st.write(
+            """With this context, EDSA is challenging you to construct a recommendation algorithm based on content or collaborative filtering, capable of accurately predicting how a user will rate a movie they have not yet viewed based on their historical preferences.""")
+        st.write(
+            """Providing an accurate and robust solution to this challenge has immense economic potential, with users of the system being exposed to content they would like to view or purchase - generating revenue and platform affinity.""")
+
+        # if st.checkbox("Problem Statement"):
+        st.subheader("Problem Statement of the Unsupervised Learning Predict")
+        st.write("Build recommender systems to recommend a movie")
+
+        # if st.checkbox("Data"):
+        st.subheader("Data Overview")
+        st.write(
+            """This dataset consists of several million 5-star ratings obtained from users of the online MovieLens movie recommendation service. The MovieLens dataset has long been used by industry and academic researchers to improve the performance of explicitly-based recommender systems, and now you get to as well!""")
+
+        st.write(
+            """For this Predict, we'll be using a special version of the MovieLens dataset which has enriched with additional data, and resampled for fair evaluation purposes.""")
+
+        st.write("""### Source:""")
+        st.write(
+            """The data for the MovieLens dataset is maintained by the GroupLens research group in the Department of Computer Science and Engineering at the University of Minnesota. Additional movie content data was legally scraped from IMDB""")
+
+        st.write("""### Supplied Files:
+        genome_scores.csv - a score mapping the strength between movies and tag-related properties. Read more here
+
+        genome_tags.csv - user assigned tags for genome-related scores
+
+        imdb_data.csv - Additional movie metadata scraped from IMDB using the links.csv file.
+
+        links.csv - File providing a mapping between a MovieLens ID and associated IMDB and TMDB IDs.
+
+        sample_submission.csv - Sample of the submission format for the hackathon.
+
+        tags.csv - User assigned for the movies within the dataset.
+
+        test.csv - The test split of the dataset. Contains user and movie IDs with no rating data.
+
+        train.csv - The training split of the dataset. Contains user and movie IDs with associated rating data.""")
+
+        # st.subheader("Raw Twitter data and label")
+        # if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+        #     st.write(raw[['sentiment', 'message']]) # will write the df to the page
     if page_selection == "EDA":
         st.header("Movie Recommender System Datasets explorer")
         st.markdown("""EDA is the Exploratory Data Analsis used to gain insight into the dataset""")
@@ -295,16 +384,96 @@ def main():
         only attract more customers to the platform but also have great customer retention as our system only gets more effective the more it is used. <br> <br> 
         Our system is easy to use and implement and will enable any company using it to rival all other similar content driving platforms. """, unsafe_allow_html=True)
 
+    elif page_selection == "User Profile":
+        st.title("User Profile")
+        st.sidebar.title("User Profile Management")
+
+        # Sidebar menu for profile management
+        profile_options = ["View Profile", "Edit Profile", "Preferences"]
+        selected_option = st.sidebar.radio("Select Option", profile_options)
+
+        name, email, age, gender = "John Doe", "john@example.com", 30, "Male"
+
+        if selected_option == "View Profile":
+            st.write("View Profile:")
+            # Placeholder for viewing user profile
+            view_profile(name, email, age, gender)
+
+        elif selected_option == "Edit Profile":
+            st.write("Edit Profile:")
+            name, email, age, gender = edit_profile()
+            # Placeholder for editing user profile
+            if st.button("Update Profile"):
+                st.success("Profile updated successfully!")
+                view_profile(name, email, age, gender)
+
+
+        elif selected_option == "Preferences":
+            st.write("Preferences:")
+            # Placeholder for managing user preferences
+            st.write("Preferred Genre: Action")
+            st.write("Favorite Actor: Tom Hanks")
+            st.write("Favorite Director: Christopher Nolan")
+
+            # Rating and feedback system
+            st.subheader("Rate and Provide Feedback for Movies:")
+            selected_movie = st.selectbox("Select a Movie:", title_list)
+            rating = st.slider("Rating (1-5)", 1, 5)
+            feedback = st.text_area("Feedback")
+
+            if st.button("Submit"):
+                # Placeholder for saving rating and feedback to database
+                st.success(
+                    f"Thank you for rating '{selected_movie}' with {rating} stars and providing feedback:\n{feedback}")
+
+    elif page_selection == "About App":
+        st.title("About the App")
+        st.write("""
+            This app is designed to provide personalized movie recommendations based on user preferences and movie ratings.
+            Users can log in, rate movies, and receive tailored recommendations to enhance their movie-watching experience.
+            """)
+
+    elif page_selection == "About Owners":
+        st.title("About the Owners")
+        st.write("""
+            This app is developed by Explore Data Science Academy as part of the Unsupervised Learning Predict project.
+            For more information about the owners and their projects, visit [Explore Data Science Academy](https://explore-datascience.net/).
+            """)
+
+        # Define owners' information
+        owners_info = [
+            {"name": "Zinhle Mjwara", "role": "Role 1", "image_url": "resources/imgs/download.jpeg"},
+            {"name": "Nkosingiphile Sefodi", "role": "Role 2", "image_url": "resources/imgs/butcher.jpg"},
+            {"name": "Jean Rabothata", "role": "Role 3", "image_url": "resources/imgs/download.jpeg"},
+            {"name": "Anele ", "role": "Role 4", "image_url": "resources/imgs/download.jpeg"},
+            {"name": "Thabang Ntuli", "role": "Role 5", "image_url": "resources/imgs/download.jpeg"},
+            {"name": "Nompumeza", "role": "Role 6", "image_url": "resources/imgs/download.jpeg"}
+        ]
+
+        # Create columns layout for displaying images and text
+        col_count = 3  # Number of columns
+        cols = st.columns(col_count)
+
+        for i, owner_info in enumerate(owners_info):
+            with cols[i % col_count]:
+                st.image(owner_info["image_url"], use_column_width=True)
+                st.write(owner_info["name"])
+                st.write(owner_info["role"])
+
+
     if page_selection == "About Us":
         st.info('**The legends who made it happen**')
-        st.write('**Jason Farrell** - Programming and Modeling')
-        st.write('**Mulalo Valencia** - Data Analysis and Documentation')
-        st.write('**Rejoice van der Walt** - Data Analysis and Documentation')
-        st.write('**Mmamolemo Matea** - Data Analysis and Documentation')
+        st.write('**Nkosingiphile Sefodi** - Programming and Modeling')
+        st.write('**Zinhle Mjwara** - Data Analysis and Documentation')
+        st.write('**Jean Rabothata** - Data Analysis and Documentation')
+        st.write('**Thabang Ntuli** - Data Analysis and Documentation')
+        st.write('**Anele** - Data Analysis and Documentation')
+        st.write('**Nompumezo** - Data Analysis and Documentation')
         st.image('resources/imgs/EDSA_logo.png',use_column_width=True)
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
+
 
 
 if __name__ == '__main__':
